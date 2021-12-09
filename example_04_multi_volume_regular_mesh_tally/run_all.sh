73d7f2e56b05ed0d105dc8576a7ae0086573b3fb
Jonathan <mail@jshimwell.com>
2021-12-09 00:09:25 +0000

    updating run script with new file names

--- example_04_multi_volume_regular_mesh_tally/run_all.sh
+++ example_04_multi_volume_regular_mesh_tally/run_all.sh
@@ -1,18 +1,15 @@
-python stage_1_create_cad.py
+
 
 
 rm *.jou *.json *.out *.xml *.exo *.h5m *.vtk *.log *.h5
 rm -rf stage_1_output/
 rm -rf stage_2_output/
 
+python 1_create_cad_and_convert_to_dagmc.py
 
-python stage_1_create_cad.py
+# needed to prevent hdf5 conflict if using cad-to-h5m
+# export HDF5_DISABLE_VERSION_CHECK=1
 
-# needed to prevent hdf5 conflict
-export HDF5_DISABLE_VERSION_CHECK=1
+python 2_run_neutronics_simulation.py
 
-python stage_2_convert_cad_to_neutronics.py
-
-mbconvert stage_2_output/unstructured_mesh.cub stage_2_output/unstructured_mesh.h5m
-
-python stage_3_run_neutronics_simulation.py
+python 3_post_process_and_plot.py
